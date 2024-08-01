f412aedbb8f7891536652a6ee9df679a8e5da555
khushvantkr12 <khushvantkumar132003@gmail.com>
2024-08-01 16:09:52 +0530

    second

--- socket/index.js
+++ socket/index.js
@@ -1,36 +1,42 @@
-import {Server} from 'socket.io';
+import { Server } from 'socket.io';
 
-const PORT = process.env.PORT || 9000 
+const PORT = process.env.PORT || 9000;
 const io = new Server(PORT, {
     cors: {
-        origin: "http://localhost:3000"
-    }
-})
+        origin: "http://localhost:3000",  // Update this to include your deployed frontend URL
+    },
+});
 
 let users = [];
 
 const addUser = (userData, socketId) => {
-    !users.some(user => user.sub == userData.sub) && users.push({...userData, socketId});
-}
+    !users.some(user => user.sub === userData.sub) && users.push({ ...userData, socketId });
+};
 
 const getUser = (userId) => {
     return users.find(user => user.sub === userId);
-}
+};
 
-io.on('connection', (socket)=>{
+const removeUser = (socketId) => {
+    users = users.filter(user => user.socketId !== socketId);
+};
+
+io.on('connection', (socket) => {
     console.log('user connected');
+
     socket.on("addUsers", userData => {
         addUser(userData, socket.id);
         io.emit("getUsers", users);
-    })
+    });
 
-    socket.on('sendMessage', data=>{
+    socket.on('sendMessage', data => {
         const user = getUser(data.receiverId);
         io.to(user?.socketId).emit('getMessage', data);
-    })
+    });
+
     socket.on('disconnect', () => {
         console.log('user disconnected');
         removeUser(socket.id);
         io.emit('getUsers', users);
-    })
-})
+    });
+});
