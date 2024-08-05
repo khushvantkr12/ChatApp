3c99ef65a4e068e8cc08bec9b098775f9ecd1ba6
khushvantkr12 <khushvantkumar132003@gmail.com>
2024-08-05 12:26:40 +0530

    second

--- socket/index.js
+++ socket/index.js
@@ -7,7 +7,6 @@ const PORT = process.env.PORT || 9000;
 const io = new Server(PORT, {
     cors: {
         origin: ["http://localhost:3000","https://chatapp-delta-bice.vercel.app/"],
-        methods: ["GET", "POST"]  
     },
 });
 
